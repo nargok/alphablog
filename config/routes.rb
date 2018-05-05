5d2f05d60d493029eacb8d878107259e6819cf09
Ryoichi Gokan <nargok@gmail.com>
2018-05-05 12:42:59 +0900

    Login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'about' => 'home#about' 
+
+  # 記事管理機能
   resources :articles
+
+  # ユーザ登録
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  # ログイン・ログアウト
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
